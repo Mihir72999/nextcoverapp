f223e9238f9c3f7487d2a0470cdcdd8a337b83ed
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-05-17 17:50:26 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Provider } from 'react-redux'
+import { AppProvider } from '../../state/context'
+import store from '../../state/redux/store'
+import Navbar from './components/Navbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <AppProvider>
+  <Provider store={store}>
+  <Navbar/>
+   <Component  {...pageProps} />
+   </Provider>
+   </AppProvider>
+    </>
 }
