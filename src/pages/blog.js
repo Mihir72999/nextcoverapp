9688032e285bf2055691924d5f93248a20db0c6e
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-05-18 19:59:55 +0530

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,16 @@
-import { Box, Container , Typography  } from '@mui/material'
+import { Box, Container , FormControl, InputLabel, MenuItem, Select, Typography  } from '@mui/material'
 import React, { useState , useEffect } from 'react'
 import { useGlobalContext } from '../../state/context'
 import styles from '../styles/Home.module.css'
 import LoadingBar from 'react-top-loading-bar'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchData } from '../../state/redux/reducer'
+import mongoose from 'mongoose'
+import Product from '../../schema/product'
 
-const blog = ({moredata}) => {
-  const [getData , setGetData] = useState(moredata.myItems.apple)
-  const [getSamsung , setGetSamsung] = useState(moredata.myItems.samsung)
+const blog = ({ myItems }) => {
+  const [getData , setGetData] = useState(myItems.apple)
+  const [getSamsung , setGetSamsung] = useState(myItems.samsung)
 const [progress, setProgress] = useState(0)
 
 
@@ -51,7 +53,7 @@ const dispatch = useDispatch()
       <div className = 'mobile' >
         <Typography className='typo' variant='h4' > Choose Your Mobile Cover </Typography>
         <Box className = 'box'>Select your model</Box>
-        <div className='select'>
+        {/* <div className='select'>
         <details>
   <summary className='summary'>{getData.brand}</summary>
   { apple?.map(e=> <p className='p' key={e} >{e}</p> ) }
@@ -64,19 +66,67 @@ const dispatch = useDispatch()
   { samsung?.map(e=> <p className='p' key={e} >{e}</p> ) }
 
 </details>
-</div>
+</div> */}
     
-      </div>
-       
-    
+      <FormControl sx={{color:'black', mx: 110, minWidth: 200 , my:1}}>
+      <InputLabel id="demo-simple-select-autowidth-label">Apple</InputLabel>
+      <Select
+    labelId="demo-simple-select-autowidth-label"
+    id="demo-simple-select-autowidth"
+    value={getData.brand}
+    label="Apple"
+    color='primary'
+    // onChange={handleChange}
+  >
+   { apple?.map((e,i) =>  <MenuItem  key={i} value={e}>{e}</MenuItem>)}
+     </Select>
+    </FormControl>
+    <FormControl sx={{color:'black', mx: 110, minWidth: 200 , my:1}}>
+      <InputLabel id="demo1-simple-select-autowidth-label">Samsung</InputLabel>
+      <Select
+    labelId="demo1-simple-select-autowidth-label"
+    id="demo1-simple-select-autowidth"
+    value={getSamsung.brand}
+    label="Samsung"
+    color='primary'
+    // onChange={handleChange}
+  >
+   { samsung?.map((e,i) =>  <MenuItem  key={i} value={e}>{e}</MenuItem>)}
+     </Select>
+    </FormControl>
+    </div>
       </>
   )
 }
 export async function getServerSideProps(contaxt){
- const data = await fetch(`${process.env.PORT}/api/addproduct`)
- const moredata = await data.json()
+  if(!mongoose.connections[0].readyState){
+    await mongoose.connect(process.env.MONGO_URI)
+  }  let productdata = await Product.find()
+    
+  let myItems = {}
+  for(let item of  productdata){
+   
+      if(item.brand in myItems){
+        if(!myItems[item.brand].brandmodel.includes(item.brandmodel) && item.availableQty > 0 ){
+          myItems[item.brand].brandmodel.push(item.brandmodel)
+        }
+        if(!myItems[item.brand].image.includes(item.image) && item.availableQty > 0 ){
+          myItems[item.brand].image.push(item.image)
+        }
+      }
+      else{
+          myItems[item.brand] = JSON.parse(JSON.stringify(item))
+          if(item.availableQty > 0 ){
+              myItems[item.brand].brandmodel = [item.brandmodel]
+              myItems[item.brand].image = [item.image]
+             
+          }
+      }
+  }
+  
+
  return{
-  props:{moredata:moredata}
+  props:{myItems:JSON.parse(JSON.stringify(myItems)) }
  }   
 }
 
