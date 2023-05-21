8fcb54491f0c4167dae5937bf939dca627887657
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-05-21 08:54:31 +0530

    Update cart.js

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,34 +1,37 @@
-'use client'
-import { Button } from "@mui/material"
+
+import { Box, Breadcrumbs, Button, Link, Typography } from "@mui/material"
 import { DeleteOutline } from "@material-ui/icons"
 import { useRouter } from "next/router"
-
+import {useGetproductQuery} from '../../state/redux/findproducts'
 import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { deletedata } from "../../state/redux/action"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import LoadingBar from 'react-top-loading-bar'
 import Image from "next/image"
+
 const cart = () => {
 
 
+    const {isFetching } = useGetproductQuery()
 
     const [datas, setDatas] = useState([])
     const [total, setTotal] = useState()
     const [progress, setProgress] = useState(0)
     const router = useRouter()
+    const {cart} = useSelector(state=>state.add)
+  
     const dispatch = useDispatch()
     const deleteCart = (e) => {
         dispatch(deletedata(e))
-        const url = `https://nextcoverapp.vercel.app/cart?deletedata=${e}`
+        const url = `http://localhost:3000/cart?deletedata=${e}`
         window.location = url
-        toast("your item deleted")
+        toast.info("your item deleted")
     }
 
-
     useEffect(() => {
-        setDatas(JSON.parse(localStorage.getItem('cart')))
+        setDatas(cart)
         setTotal(JSON.parse(localStorage.getItem('subtotal')))
 
 
@@ -39,38 +42,46 @@ const cart = () => {
   
       setProgress(100)
      },[])
-    
-    const goBack = () => {
-        router.push('case-soft-silicon-cover') 
-        
-    }
+     if(isFetching){
+        return <LoadingBar className='loadingbar' height={63}  color='#FFFFFF'  progress={progress} waitingTime={800} onLoaderFinished={() => setProgress(0)}/>
+        }    
+   
 const checkout = () => {
     router.push('checkoutpay') 
 
 }
     return (
         <>
-         <LoadingBar className='loadingbar' height={63}  color='#FFFFFF'  progress={progress} waitingTime={800} onLoaderFinished={() => setProgress(0)}/>
        <ToastContainer />
-            {datas < 1 && <div className="cart_title">No Item in Cart</div>}
-            {
-                datas && Object.values(datas).map((e, i) => {
+       <Box display='flex' justifyContent='center'  mt={10}>
+            <Breadcrumbs area-label='breadcrumb'>
+                <Link underline="hover" href="/">Home</Link>
+                <Link underline="hover" href="/case-soft-silicon-cover">Case</Link>
+               
+                <Typography color='text.primary' >cart</Typography> 
+            </Breadcrumbs>
+
+           </Box>
+            {datas.length === 0 && <div className="cart_title">No Item in Cart</div>}
+            { 
+                datas && datas.map((e, i) => {
                     return <div className="cart" key={i}>
+                           
                         <div className='cart_image'><Image src={e.image} height={70} width={50} alt='' /></div>
                         <div className='cart_select'>{e.select}</div>
                         <div className="cart_qtyprice">
                         <div className="cart_qty">qty:{e.qty}</div>
                         <div className='cart_price'>₹{e.amount}</div>
                         </div>
-                        <div className='cart_name'>{e.name.slice(0, 20)}...</div>
-                        <div className='cart_icon'>  <DeleteOutline onClick={() => deleteCart(e._id)} /></div>
+                        <div className='cart_name'>{e.name}</div>
+                        <div className='cart_icon'>  <DeleteOutline onClick={() => deleteCart(e.select)} /></div>
 
                     </div>
                 })
             }
-            {datas  && <div className="cart_subtotal">subTotal = ₹{total}</div>}
-            {datas && <div onClick={checkout} className='cart_button'><Button color='primary' variant='contained'>checkOut</Button></div>}
-            {datas < 1 && <Button onClick={goBack} style={{ margin: '0 40%' }} color='secondary' variant='contained'>Go back</Button>}
+            {datas.length > 0  && <div className="cart_subtotal">subTotal = ₹{total}</div>}
+            {datas.length > 0 && <div onClick={checkout} className='cart_button'><Button color='primary' variant='contained'>checkOut</Button></div>}
+           
         </>
     )
 }
