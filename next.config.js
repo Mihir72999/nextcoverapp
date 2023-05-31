78e686d0bbf545dc80f2d5e54da4b8950046f1f6
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-05-31 13:22:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimization
     remotePatterns: [
+    unoptimized: true,
       {
         protocol: 'https',
         hostname: 'zapvi.in',
        
       }
     ]
-  }
+  },
+      output: 'npm export'
+    
 }
 module.exports = nextConfig
