e776b52c7bb51739faa67f358bb52304a3bc5035
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-05-31 13:24:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimization
-    remotePatterns: [
     unoptimized: true,
+    remotePatterns: [
+    
       {
         protocol: 'https',
         hostname: 'zapvi.in',
