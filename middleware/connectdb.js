6bde0366d629224c2ae61435d93955d1fea1b074
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-06-01 19:24:43 +0530

    Update connectdb.js

--- middleware/connectdb.js
+++ middleware/connectdb.js
@@ -5,7 +5,7 @@ const connectdb= handler => async(req , res) =>{
     if(mongoose.connections[0].readyState){
     return handler(req,res)
     }
-   await  mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser:true,useUnifiedTopology:true,
+   await  mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser:true,useUnifiedTopology:true,
     })
     console.log("mongodb connected")
      return handler(req,res)  
